7eb54ec0537beac6207915ee15732948bdbe83cc
Noah Bouter <noah.boutter@etu.minesparis.psl.eu>
2026-01-30 11:03:26 +0100

    Update grid type

--- grid.py
+++ grid.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+import entities as en
+
 class Grid:
     def __init__(self, n):
-        self.mat = np.zeros((n,n), dtype=np.int)
-        self.age = 0
+        self.mat = [[0 for i in range(n)] for i in range(n)]
 
     def list_adj(self, i, j):
         pass
