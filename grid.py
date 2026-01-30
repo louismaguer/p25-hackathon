d35849b8e0bfa47d6ae748fdb5c815fef6e209a3
Noah Bouter <noah.boutter@etu.minesparis.psl.eu>
2026-01-30 11:07:38 +0100

    add list_adj

--- grid.py
+++ grid.py
@@ -7,5 +7,16 @@ class Grid:
         self.mat = [[0 for i in range(n)] for i in range(n)]
 
     def list_adj(self, i, j):
-        pass
+        ans = []
+        n = len(self.mat)
+        if (i - 1) >= 0:
+            ans.append((i - 1, j))
+        if (j - 1) >= 0:
+            ans.append((i, j - 1))
+        if (i + 1) < n:
+            ans.append((i + 1, j))
+        if (j + 1) < n:
+            ans.append((i, j + 1))
+        return ans
+
 
