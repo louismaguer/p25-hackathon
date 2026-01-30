12114c5156fb4f8226d7abed488d214bf3972d28
Noam Guillou <noam.guillou@etu.minesparis.psl.eu>
2026-01-30 13:09:14 +0100

    affichage graphique fixe

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import matplotlib.pyplot as plt
 from matplotlib import colors
+import entities as en 
 
-# --- TA FONCTION ---
-def afficher_matrice_facile(matrice, liste_couleurs):
+def afficher_matrice(matrice_animaux, matrice_herbe, liste_couleurs):
+    nouvelle_matrice = conversion_état(matrice_animaux, matrice_herbe):
     palette = colors.ListedColormap(liste_couleurs)
-    plt.imshow(matrice, cmap=palette)
+    plt.imshow(nouvelle_matrice, cmap=palette)
     plt.show()
 
-# --- TES DONNÉES ---
-# Une matrice 3x3 (comme dans ton premier essai)
-ma_grille = [
-    [0, 1, 2],
-    [2, 0, 1],
-    [1, 2, 0]
-]
-
-# On définit que : 0=Noir, 1=Gris, 2=Vert
-mes_couleurs = ['black', 'whitesmoke', 'green']
-
-# --- ON LANCE L'AFFICHAGE ---
-afficher_matrice_facile(ma_grille, mes_couleurs)
-
 def conversion_état(matrice_animaux, matrice_herbe):
-    
+    matrice = [[0 for k in range(len(matrice_animaux[0]))] for i in range(len(matrice_animaux))]
+    for r in range(len(matrice_animaux[0])):
+        for c in range(len(matrice_animaux[0])):
+            if isinstance(matrice_animaux[r][c], en.Wolf) :
+                matrice[r][c] = 3
+            elif isinstance(matrice_animaux[r][c], en.Sheep) :
+                matrice[r][c] = 1
+    for r in range(len(matrice_herbe[0])):
+        for c in range(len(matrice_herbe[0])):
+            if isinstance(matrice_animaux[r][c], en.Grass) and matrice[r][c] = 0 :
+                if matrice_animaux[r][c].temps_repousse == 0 :
+                    matrice[r][c] = 2
+    return matrice
+
+
+
+mes_couleurs = ['almond', 'whitesmoke', 'green', 'black']
+
