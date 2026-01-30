344abd6f8c2a91d1c49dff83b80cc2a24be759df
Noam Guillou <noam.guillou@etu.minesparis.psl.eu>
2026-01-30 14:04:42 +0100

    khaki

--- testpournoam.py
+++ testpournoam.py
@@ -1,10 +1,18 @@
 from random import sample
+import entities as en
 
 def herbe_aleatoire(n):
-    units=[(line,col) for col in range(n) for line in range(n)]
-    ntrees=int(n**2*p)
-    trees=sample(units,ntrees)
-    states=[[0]*n for _ in range(n)]
-    for (i,j) in trees:
-        states[i][j]=1
-    return states
+    case=[(line,col) for col in range(n) for line in range(n)]
+    proba_grass = 0.3
+    nb_grass=int(n**2*proba_grass)
+    grass=sample(case,nb_grass)
+    matrice_herbe=[[0]*n for _ in range(n)]
+    for (i,j) in grass :
+        matrice_herbe[i][j]=1
+    return matrice_herbe
+
+def animaux_aleatoire(n):
+    matrice_animaux = [[0]*n for _ in range(n)]
+    nb_sheep = 50
+    nb_wolf = 10 
+    
