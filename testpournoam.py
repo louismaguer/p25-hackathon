62643bde0425ea9642705012fb7436c9fa033cf8
Noam Guillou <noam.guillou@etu.minesparis.psl.eu>
2026-01-30 14:27:41 +0100

    aléatoire

--- testpournoam.py
+++ testpournoam.py
@@ -1,5 +1,6 @@
 from random import sample
 import entities as en
+import random
 
 def herbe_aleatoire(n):
     case=[(line,col) for col in range(n) for line in range(n)]
@@ -13,6 +14,15 @@ def herbe_aleatoire(n):
 
 def animaux_aleatoire(n):
     matrice_animaux = [[0]*n for _ in range(n)]
+    case=[(line,col) for col in range(n) for line in range(n)]
     nb_sheep = 50
     nb_wolf = 10 
-    
+    echantillon = sample(case, nb_sheep + nb_wolf)
+    for (i,j) in echantillon[:50] :
+        matrice_animaux[i][j] = en.Sheep(i,j,0, en.SHEEP_INITIAL_ENERGY)
+    for (i,j) in echantillon[50:] :
+        matrice_animaux[i][j] = en.Wolf(i,j,0, en.WOLF_INITIAL_ENERGY)
+    return matrice_animaux
+
+print(herbe_aleatoire(9))
+print(animaux_aleatoire(9))
