49260acdc12c0343e02eb736429fc5bbc4f315f9
Lukas Fermaut <lukas.fermaut@etu.minesparis.psl.eu>
2026-01-30 13:15:22 +0100

    regler erreur

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from matplotlib import colors
 import entities as en 
 
 def afficher_matrice(matrice_animaux, matrice_herbe, liste_couleurs):
-    nouvelle_matrice = conversion_état(matrice_animaux, matrice_herbe):
+    nouvelle_matrice = conversion_état(matrice_animaux, matrice_herbe)
     palette = colors.ListedColormap(liste_couleurs)
     plt.imshow(nouvelle_matrice, cmap=palette)
     plt.show()
@@ -18,7 +18,7 @@ def conversion_état(matrice_animaux, matrice_herbe):
                 matrice[r][c] = 1
     for r in range(len(matrice_herbe[0])):
         for c in range(len(matrice_herbe[0])):
-            if isinstance(matrice_animaux[r][c], en.Grass) and matrice[r][c] = 0 :
+            if isinstance(matrice_animaux[r][c], en.Grass) and matrice[r][c] == 0 :
                 if matrice_animaux[r][c].temps_repousse == 0 :
                     matrice[r][c] = 2
     return matrice
