6f634353d467803665c08c2ac333e49016cab904
Lukas Fermaut <lukas.fermaut@etu.minesparis.psl.eu>
2026-01-30 11:54:15 +0100

    1ere modif de uptade wolf

--- grid.py
+++ grid.py
@@ -1,5 +1,6 @@
 import numpy as np
 import entities as en
+import random
 
 class Grid:
     def __init__(self, n):
@@ -22,7 +23,16 @@ class Grid:
         pass
 
     def update_wolf(self, i, j):
-        pass
+        Wolf=self.mat[i][j]
+        adj=self.list_adj(self,i,j)
+        Sheep=[k for k in adj if isinstance(self.mat[k[0]][k[1]],en.Sheep)]
+        if Sheep:
+            nx,ny=random.choice(Sheep)
+            self.mat[i][j]=0
+            self.mat[nx][ny]=Wolf
+            Wolf.move(nx,ny)
+        else: 
+            Vide=[k for k in adj if self.mat[k[0]][k[1]]==0]
 
     def update_sheep(self, i, j):
         pass
