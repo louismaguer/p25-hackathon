1fae42db5da024fe48f5f85e1d297dd1720b5032
Lukas Fermaut <lukas.fermaut@etu.minesparis.psl.eu>
2026-01-30 12:25:56 +0100

    Wolf reproduction done

--- grid.py
+++ grid.py
@@ -22,16 +22,23 @@ class Grid:
     
 
     def update_wolf(self, i, j):
-        Wolf=self.mat[i][j]
+        wolf_=self.mat[i][j]
         adj=self.list_adj(self,i,j)
-        Sheep=[k for k in adj if isinstance(self.mat[k[0]][k[1]],en.Sheep)]
-        if Sheep:
-            nx,ny=random.choice(Sheep)
+        sheep_coos=[k for k in adj if isinstance(self.mat[k[0]][k[1]],en.Sheep)]
+        if sheep_coos:
+            nx,ny=random.choice(sheep_coos)
             self.mat[i][j]=0
-            self.mat[nx][ny]=Wolf
-            Wolf.move(nx,ny)
+            self.mat[nx][ny]=wolf_
+            wolf_.move(nx,ny)
+            wolf_.energie += en.WOLF_ENERGY_FROM_SHEEP
         else: 
             Vide=[k for k in adj if self.mat[k[0]][k[1]]==0]
+            if Vide: 
+                nx,ny=random.choice(Vide)
+                self.mat[i][j]=0
+                self.mat[nx][ny]=wolf_
+                wolf_.move(nx,ny)
+        wolf_.energie -=en.WOLF_ENERGY_LOSS_PER_TURN
 
     def update_sheep(self, i, j):
         pass
@@ -51,7 +58,20 @@ class Grid:
         pass
 
     def reproduct_wolf(self):
-        pass
+        wolf_coo=[]
+        for i in range (len(self.mat)):
+            for j in range (len(self.mat)):
+                if isinstance(self.mat[i][j],en.Wolf):
+                    wolf_coo.append((i,j))
+        for (k,l) in wolf_coo:
+            parents=self.mat[k][l]
+            if parents.can_reprod():
+                A=self.list_adj(k,l)
+                Vide=[v for v in A if self.mat[v[0]][v[1]]==0]
+                if Vide:
+                    nx,ny=random.choice(Vide)
+                    self.mat[nx][ny]=en.Wolf(nx,ny,0,en.WOLF_INITIAL_ENERGY)
+                    parents.energie -= en.REPRODUCTION_ENERGY_COST
 
     def update_age(self):
         n = len(self.mat)
